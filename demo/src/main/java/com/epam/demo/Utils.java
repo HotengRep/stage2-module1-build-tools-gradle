d8134c6e3dbadffe0b5381380400b4513c5d53b0
Makar <hoteng789@gmail.com>
2023-09-30 19:21:03 +0400

    Gradle v2 try catch

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,10 @@
 package com.epam.demo;
 
 import java.util.List;
+import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       return args.stream().allMatch(x->Integer.parseInt(x) > 0);
+       return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,14 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return Integer.valueOf(str) > 0;
+
+      try {
+          return Double.parseDouble(str) > 0;
+      }
+      catch (NumberFormatException exception)
+      {
+          return  false;
+      }
+
     }
 }
